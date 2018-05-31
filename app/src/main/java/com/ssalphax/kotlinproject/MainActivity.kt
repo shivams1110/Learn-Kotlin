a7b2c8f82e32bb2e594f14bf4b777451d8004d41
shivams1110 <shivams1110@live.in>
2018-05-31 12:55:19 +0530

    first Commit

--- app/src/main/java/com/ssalphax/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/ssalphax/kotlinproject/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
 
-        
+
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
